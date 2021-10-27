f31cc6be74778ffaedc6e3b7d1f0ab26ffc4b5ae
stevenrdz <stevenrodriguezm97@gmail.com>
2021-10-27 17:18:36 -0500

    Avance de gifsApp

---
+++ gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,31 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+  styles: [
+  ]
+})
+export class BusquedaComponent{
+
+  @ViewChild('txtBuscar') txtBuscar!:ElementRef<HTMLInputElement>;
+
+  constructor( private GifsService: GifsService){}
+
+  buscar(){
+
+    const valor= this.txtBuscar.nativeElement.value;
+
+    if (valor.trim().length === 0){
+      return;
+    }
+    
+
+    this.GifsService.buscarGifs(valor);
+
+    this.txtBuscar.nativeElement.value = '';
+  }
+  
+
+}
